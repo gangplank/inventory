69b61a2ccb503867c1f4ffdac823e575c83770a6
Matthew Sumner <matt.m.sumner@gmail.com>
2013-10-19 10:33:41 -0700

    Initial commit with first test.

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+
+gem 'pg'
+gem 'high_voltage'
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'debugger'
+end
+
+group :test do
+  gem 'factory_girl_rails'
+end
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inventory::Application.config.secret_key_base = '274b676661e2696c0bd75d7923f38fc2f683081024bc380300c47f0290dd5528e2e96a95911efa3a8ed8f502207f25132d9aaf90ddf96e42a03d4ec4efea4a46'
